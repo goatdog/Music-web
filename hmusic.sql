0bd14ee568c6abac4980db187a8f5e315ec561c1
goatdog <100104189+goatdog@users.noreply.github.com>
2022-12-07 21:51:05 +0700

    Update hmusic.sql

--- hmusic.sql
+++ hmusic.sql
@@ -36,19 +36,6 @@ CREATE TABLE `artist` (
   `artist_photo` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `artist`
---
-
-INSERT INTO `artist` (`artist_id`, `artist_name`, `artist_biography`, `artist_details`, `artist_photo`) VALUES
-(1, 'Eddy kenzo', 'Edrisah Musuuza, known professionally as Eddy', NULL, '1592126820_66799837321181_eddy_kenzo.jpeg'),
-(3, 'Bebe Cool', 'Bebe Cool is an African reggae and ragga musi', NULL, '1592129400_3978146945942_WxhdSuakJ4wZhi6atGkW2Qyt.jpeg'),
-(4, 'Sheebah Karungi', 'Sheebah Karungi is a Ugandan recording artist', NULL, '1592129479_31892627593739_howwebiz_88e723f97cde9c8b7eb0aaaabcacbe51_1464525504_cover.jpg'),
-(5, 'Jizzle', 'Jizzle was influenced by artist like Lil Wayn', NULL, '1592902550_15623277316181_IMG_1965.jpeg'),
-(6, 'St. Brikama Boyo', 'ST Da Gambian Dream or Brikama Boyo was Born ', NULL, '1593167104_83132164437012_boyo.png'),
-(7, 'Apass', 'Alexander Bagonza, better known by his stage ', NULL, '1593167348_9697153643255_song_pic_(1).png'),
-(8, 'Chamsia Sagaf', 'Chamsia Sagaf (born 1955) is a Comoros singer', NULL, '1594049894_53755335761047_Chamsia-Sagaf.jpg');
-
 -- --------------------------------------------------------
 
 --
@@ -79,17 +66,6 @@ CREATE TABLE `songs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `songs`
---
-
-INSERT INTO `songs` (`song_id`, `song_mp3`, `song_photo`, `song_date`, `aritst_id`, `song_name`, `upload_by`) VALUES
-(12, '1594313558_76984949380544_1593167375_54137122780950_Eddy_Kenzo_-_Tweyagale_.mp3', '1594313612_3327764631838_1592131027_41742619472167_maxresdefault.jpg', NULL, '1', 'Tweyaagale', 0),
-(13, '1604311637_36558925654713_y2mate.com_-_Shena_Skies_-_Ayagadde_Mune_%28Official_Video%29_fPbCQVtFtC0.mp3', '1604311637_36715812392813_abushir-1.jpg', NULL, '8', 'Simple Song', 8),
-(14, '1604312614_2871382418172_1593507781_25997519201764_Harmonize_%26_Sheebah_-_Follow_Me.mp3', '1604312614_67986542211751_101954524_1183313058714256_4505932558890534420_n.jpg', NULL, '7', 'Another Song', 8);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
@@ -105,22 +81,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `username`, `password`, `first_name`, `last_name`, `reg_date`, `last_seen`, `photo`) VALUES
-(1, 'haddy1', '$2y$10$Zt12am7RJv/ZR7ZrS.2kIONx4VZJ7deAhsLV74', 'Hadijatou', NULL, NULL, '1591095139', ''),
-(2, 'admin_2', '$2y$10$T1yu8zxLMJQg8.FPrQPpLuIUFxyuTihfCFihtM', 'Hamoud', NULL, NULL, '1591522035', ''),
-(3, 'admin_3', '$2y$10$hbW2BB4zqvUBQybzW8jh2e6v.j4NSBO.irP.YM', 'Hamoud', NULL, NULL, '1591525236', ''),
-(4, 'admin_4', '$2y$10$a3AHQtlfWeaUks.bEA2TQebLZ9i5MiT557HmJb', 'Biirah', NULL, NULL, '1591525821', ''),
-(5, 'admin_5', '$2y$10$dstP./.91eJNNoNwpsXlBOl98dRQqdyA8JOu9p', 'Hadijatou', NULL, NULL, '1591526196', ''),
-(6, 'haddy6', '$2y$10$K2LQuWSph7BQZCUE2Hkj0u7JmXsCJZC1AosX6D', 'Hadijatou', NULL, NULL, '1591952978', ''),
-(7, 'haddy7', '$2y$10$Sq9VYUX9Dp8ta5HzwWTdbuP6vU8fPu33YMmTx2eWjTRhEQSOB8vPK', 'haddy', NULL, NULL, '1591953667', ''),
-(8, 'Criss', '$2y$10$9Pelyte0jzxj6QW49v/8Y.0zOJU0lREPHr9Jfe2KJ7ypYFNdNWhie', 'Criss', NULL, NULL, '1604310887', '');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `views`
 --
 
@@ -131,19 +91,6 @@ CREATE TABLE `views` (
   `view_time` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `views`
---
-
-INSERT INTO `views` (`view_id`, `user_id`, `song_id`, `view_time`) VALUES
-(37, 7, 12, '1594313565'),
-(38, 7, 12, '1594313619'),
-(39, 7, 12, '1594313634'),
-(40, 8, 12, '1604312628');
-
---
--- Indexes for dumped tables
---
 
 --
 -- Indexes for table `artist`
